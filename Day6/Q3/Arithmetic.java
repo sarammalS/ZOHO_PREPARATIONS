39650655ce6cf8420827f0e1d83b4c9e876965b7
sarammalS <sarammalbca2003@gmail.com>
2025-07-28 18:59:10 +0530

    Add files via upload

---
+++ Day6/Q3/Arithmetic.java
@@ -0,0 +1,22 @@
+package arith;
+public class Arithmetic{
+    public int add(int a,int b){
+        return a+b;
+
+    }
+    public int sub(int a,int b){
+        return Math.abs(a-b);
+        
+    }
+    public int mul(int a,int b){
+        return a*b;
+        
+    }
+    public int div(int a,int b){
+        return a/b;
+        
+    }
+    public int mod(int a,int b){
+        return a%b;
+    }
+}
