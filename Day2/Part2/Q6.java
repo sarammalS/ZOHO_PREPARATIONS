33790800ea47ca34764d987a2f72ee4cc908ee23
sarammalS <sarammalbca2003@gmail.com>
2025-07-28 10:09:27 +0530

    Update Q6.java

--- Day2/Part2/Q6.java
+++ Day2/Part2/Q6.java
@@ -1,3 +1,7 @@
+/*
+5. Write a program to initialize the following character arrays and print a suitable message after checking the arrays whether the two arrays are identical or not. Make suitable use of Boolean data type. X[] =('m', 'n', 'o', 'p'} and Y[] =('m', 'n', 'o', 'p' }
+X[] =('m', 'n', 'o', 'p'} and
+Y[] =('m', 'n', 'o', 'p' }*/
 public class Q6 {
     public static void main(String[] args) {
         char[] X = {'m', 'n', 'o', 'p'};
