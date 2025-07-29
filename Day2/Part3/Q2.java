e9f086a3b74fef38eccd653b8a214409b97fe22c
sarammalS <sarammalbca2003@gmail.com>
2025-07-29 13:58:14 +0530

    Update Q2.java

--- Day2/Part3/Q2.java
+++ Day2/Part3/Q2.java
@@ -1,3 +1,23 @@
+/*2. Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+‌
+For example:
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28
+...
+‌
+Example 1:
+Input: columnNumber = 1
+Output: "A"
+‌
+Example 2:
+Input: columnNumber = 28
+Output: "AB"
+‌*/
 public class Q2 {
     public static String convertToTitle(int n) {
         StringBuilder sb = new StringBuilder();
