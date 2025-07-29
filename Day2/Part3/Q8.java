2f583333f606b8be42a667fc766e04eb3a7cf3e2
sarammalS <sarammalbca2003@gmail.com>
2025-07-29 14:03:25 +0530

    Update Q8.java

--- Day2/Part3/Q8.java
+++ Day2/Part3/Q8.java
@@ -1,3 +1,19 @@
+/*8. We define the usage of capitals in a word to be right when one of the following cases holds:
+1. All letters in this word are capitals, like "USA".
+2. All letters in this word are not capitals, like "leetcode".
+3. Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+Example 1:
+Input: word = "USA"
+Output: true
+‌
+Example 2:
+Input: word = "FlaG"
+Output: false
+‌
+Constraints:
+1 <= word.length <= 100
+word consists of lowercase and uppercase English letters.*/
 public class Q8 {
     public static boolean isValidCap(String w) {
         return w.equals(w.toUpperCase()) ||
