dc07b41f6facbb6bd240ae5239249a3bc2dd9b5e
sarammalS <sarammalbca2003@gmail.com>
2025-07-29 13:56:33 +0530

    Update Q1.java

--- Day2/Part3/Q1.java
+++ Day2/Part3/Q1.java
@@ -1,3 +1,14 @@
+/*. Given two binary strings a and b, return their sum as a binary string.
+Input: a = "11", b = "1"
+Output: "100"
+‌
+Input: a = "1010", b = "1011"
+Output: "10101"
+‌
+Constraints:
+1 <= a.length, b.length <= 10^4
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.*/
 public class Q1 {
     public static String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
@@ -14,7 +25,8 @@ public class Q1 {
     }
 
     public static void main(String[] args) {
-        System.out.println(addBinary("11", "1"));     // 100
-        System.out.println(addBinary("1010", "1011")); // 10101
+        System.out.println(addBinary("11", "1"));
+        System.out.println(addBinary("1010", "1011")); 
     }
 }
+//// 10101
