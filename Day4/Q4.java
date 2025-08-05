9467c630b00896d107dbbb3fa943798e60e1b677
sarammalS <sarammalbca2003@gmail.com>
2025-08-05 14:20:08 +0530

    Update Q4.java

--- Day4/Q4.java
+++ Day4/Q4.java
@@ -1,16 +1,29 @@
 /*4. Write a program to illustrate that Classes cannot be used for multiple Inheritance.*/
+// Base class A
 class A {
-    void show() {
-        System.out.println("A");
+    void displayA() {
+        System.out.println("Class A method");
     }
 }
 
+// Base class B
 class B {
-    void show() {
-        System.out.println("B");
+    void displayB() {
+        System.out.println("Class B method");
     }
 }
 
-// This will cause error: class C cannot extend A and B
-// class C extends A, B {
+// Trying to inherit both A and B
+// This will cause a **COMPILATION ERROR**
+// class C extends A, B { // Not allowed in Java
+//     void displayC() {
+//         System.out.println("Class C method");
+//     }
 // }
+
+public class MultipleInheritanceDemo {
+    public static void main(String[] args) {
+        System.out.println("Java does NOT support multiple inheritance with classes directly.");
+        System.out.println("Uncommenting the line 'class C extends A, B' will cause a compilation error.");
+    }
+}
